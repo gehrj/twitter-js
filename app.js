9ed6d715d8fe944eadc905e7c10043b7c3a17c6a
Justin Gehr <gehrjay@gmail.com>
2017-04-25 13:41:24 -0500

    got through part of sockets

--- app.js
+++ app.js
@@ -3,19 +3,30 @@ const app = express();
 const volleyBall = require('volleyball');
 const nunjucks = require('nunjucks');
 const routes = require('./routes');
+const bodyParser = require('body-parser');
+const socketio = require('socket.io');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(volleyBall);
 
 app.use(express.static('public'))
-app.use('/', routes);
+
+
 
 app.set('view engine','html');
 app.engine('html', nunjucks.render);
 nunjucks.configure('views',{noCache: true});
 
 
-app.use(volleyBall);
 
 
 
-app.listen(3000,function(){
+
+let server = app.listen(3000,function(){
     console.log('The Server is Up!');
 });
+
+let io = socketio.listen(server);
+
+app.use('/', routes(io));
--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,38 @@
-const express = require('express');
-const router = express.Router();
-// could use one line instead: const router = require('express').Router();
-const tweetBank = require('../tweetBank');
-const path = require('path')
+module.exports = function (io) {
+  const express = require('express');
+  const router = express.Router();
+  // could use one line instead: const router = require('express').Router();
+  const tweetBank = require('../tweetBank');
+  const path = require('path')
 
-router.get('/users/:name', function(req, res) {
-  var name = req.params.name;
-  var tweets = tweetBank.find( {name: name} );
-  res.render( 'index', { tweets: tweets } );
-});
+  router.get('/users/:name', function(req, res) {
+    var name = req.params.name;
+    var tweets = tweetBank.find( {name: name} );
+    res.render( 'index', { tweets: tweets } );
+  });
 
-router.get('/', function (req, res) {
-  let tweets = tweetBank.list();
-  res.render( 'index', { tweets: tweets } );
-});
+  router.get('/tweets/:id', function(req,res) {
+    var id = parseInt(req.params.id);
+    var tweets = tweetBank.find({id: id});
+    res.render('index', {tweets: tweets});
+
+  })
+
+  router.get('/', function (req, res) {
+    let tweets = tweetBank.list();
+    res.render( 'index', { tweets: tweets, showForm: true } );
+  });
+
+  router.post('/tweets', function(req, res) {
+    var name = req.body.name;
+    var text = req.body.text;
+    tweetBank.add(name, text);
+    io.sockets.emit('newTweet', {name,text});
+    res.redirect('/');
+  });
 
 
 
-module.exports = router;
+
+return router;
+};
