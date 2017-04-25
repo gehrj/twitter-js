4265903a624202698cc559ca0c5b5e9f6cbab5d3
Rob Tong <robtong21@gmail.com>
2017-04-25 10:48:53 -0500

    indiv users tweets done

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const express = require('express');
 const app = express();
 const volleyBall = require('volleyball');
 const nunjucks = require('nunjucks');
+const routes = require('./routes');
 
-const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
-
+app.use(express.static('public'))
+app.use('/', routes);
 
 app.set('view engine','html');
 app.engine('html', nunjucks.render);
@@ -12,17 +13,8 @@ nunjucks.configure('views',{noCache: true});
 
 
 app.use(volleyBall);
-app.get('/',function(req,res) {
-    res.render( 'index', {title: 'Hall of Fame', people: people} );
-})
 
-app.get('/news',function(req,res) {
-    res.send('News Page');
-})
 
-// app.get('/special',function(req,res) {
-//     res.send('test');
-// })
 
 app.listen(3000,function(){
     console.log('The Server is Up!');
---
+++ routes/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+// could use one line instead: const router = require('express').Router();
+const tweetBank = require('../tweetBank');
+const path = require('path')
+
+router.get('/users/:name', function(req, res) {
+  var name = req.params.name;
+  var tweets = tweetBank.find( {name: name} );
+  res.render( 'index', { tweets: tweets } );
+});
+
+router.get('/', function (req, res) {
+  let tweets = tweetBank.list();
+  res.render( 'index', { tweets: tweets } );
+});
+
+
+
+module.exports = router;
